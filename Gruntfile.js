24b5db95615ba846712b28e8984fedbfcaf87d01
JacobWay <soarinblue@gmail.com>
2015-06-05 16:15:47 +0800

    add grunt uglify, beautify, watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,18 +5,35 @@ module.exports = function(grunt){
         pkg: grunt.file.readJSON("package.json"),
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
             },
             build: {
                 src: 'src/main.js',
                 dest: 'build/<%= pkg.name %>.min.js'
+            },
+            my_advance_target: {
+                options: {
+                    beautify: {
+                        width: 80,
+                        beautify: true
+                    }
+                },
+                files: {
+                    'build/<%= pkg.name %>.beauty.js': ['src/main.js']
+                }
             }
         },
+
+        watch: {
+            files: ['src/**/*.js'],
+            tasks: ['uglify',]
+        }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default tasks
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify', 'watch']);
 };
